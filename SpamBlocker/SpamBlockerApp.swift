9cf737bf9c060066f555989f128ddd63510f54ee
HyeonsooKim <hyeonsookim2@gmail.com>
2026-04-25 22:38:53 +0900

    Initial Commit

---
+++ SpamBlocker/SpamBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpamBlockerApp.swift
+//  SpamBlocker
+//
+//  Created by hyeonsoo on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SpamBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
